9b3be3d7cad80a29b85eec15d53c7b1cf042e7ad
Alexandros Polymeros <alpolymer@gmail.com>
2023-11-13 21:48:05 +0200

    otherCurrency

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,31 @@ import { useState } from "react"
 import { Listbox } from "@headlessui/react"
 
 const currencies = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
+  { id: 1, name: "Euro" },
+  { id: 2, name: "US Dollar" },
+  { id: 3, name: "British Pound" },
+  { id: 4, name: "Japanese Yen" },
+  { id: 5, name: "Australian Dollar" },
 ]
 function App() {
+  const [selectedCurrency, setSelectedCurrency] = useState(currencies[0])
+  const [otherCurrency, setOtherCurrency] = useState(currencies[1])
+
   return (
     <>
       <main className="flex min-h-screen justify-between ">
         <div className="bg-red-500 w-1/2 text-center">
-          <MyListbox />
+          <MyListbox
+            selectedCurrency={selectedCurrency}
+            onSelectCurrency={setSelectedCurrency}
+          />
         </div>
 
         <div className="bg-blue-500 w-1/2 text-center">
-          <MyListbox />
+          <MyListbox
+            selectedCurrency={otherCurrency}
+            onSelectCurrency={setOtherCurrency}
+          />
         </div>
       </main>
     </>
@@ -26,20 +35,18 @@ function App() {
 
 export default App
 
-function MyListbox() {
-  const [selectedPerson, setSelectedPerson] = useState(currencies[0])
-
+function MyListbox({ selectedCurrency, onSelectCurrency }) {
   return (
-    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+    <Listbox value={selectedCurrency} onChange={onSelectCurrency}>
+      <Listbox.Button>{selectedCurrency.name}</Listbox.Button>
       <Listbox.Options>
-        {currencies.map((person) => (
-          <Listbox.Option key={person.id} value={person}>
-            {person.name}
+        {currencies.map((currency) => (
+          <Listbox.Option key={currency.id} value={currency}>
+            {currency.name}
           </Listbox.Option>
         ))}
       </Listbox.Options>
-      <p className="text-orange-500">{selectedPerson.name}</p>
+      <p className="text-orange-500">{selectedCurrency.name}</p>
     </Listbox>
   )
 }
