91c0ef6d6dee59172f694d7009084ea4de11eb05
Alexandros Polymeros <alpolymer@gmail.com>
2023-11-13 16:20:19 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <section className="bg-blue-300 p-4 rounded-lg">
+          <h1 className="text-3xl font-poppins ">Currency Converter</h1>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
